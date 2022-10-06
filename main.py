81b980d1881b023189a6d69c393253f88aa1e24a
jkrue242 <josephkrueger242@gmail.com>
2022-10-05 23:31:23 -0500

    remove unnecessary comments

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ while True:
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
     for (x, y, w, h) in faces:
-        # print(x,y,w,h)
         roi_gray = gray[y:y + h, x:x + w]
         roi_color = frame[y:y + h, x:x + w]
 
